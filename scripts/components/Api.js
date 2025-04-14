6c15e9725f3f387f878c55500985936b65a1a51d
Phellippe <phellippef@gmail.com>
2025-04-14 18:06:31 -0300

    atualizacao sprint 12

---
+++ scripts/components/Api.js
@@ -0,0 +1,70 @@
+export default class Api {
+    constructor({ baseUrl, headers }) {
+      this._baseUrl = baseUrl;
+      this._headers = headers;
+    }
+  
+    _handleResponse(res) {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    }
+  
+    getUserInfo() {
+      return fetch(`${this._baseUrl}/users/me`, {
+        headers: this._headers
+      }).then(this._handleResponse);
+    }
+  
+    getInitialCards() {
+      return fetch(`${this._baseUrl}/cards`, {
+        headers: this._headers
+      }).then(this._handleResponse);
+    }
+  
+    setUserInfo({ name, about }) {
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({ name, about })
+      }).then(this._handleResponse);
+    }
+  
+    setAvatar({ avatar }) {
+      return fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({ avatar })
+      }).then(this._handleResponse);
+    }
+  
+    addCard({ name, link }) {
+      return fetch(`${this._baseUrl}/cards`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({ name, link })
+      }).then(this._handleResponse);
+    }
+  
+    deleteCard(cardId) {
+      return fetch(`${this._baseUrl}/cards/${cardId}`, {
+        method: 'DELETE',
+        headers: this._headers
+      }).then(this._handleResponse);
+    }
+  
+    likeCard(cardId) {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: 'PUT',
+        headers: this._headers
+      }).then(this._handleResponse);
+    }
+  
+    unlikeCard(cardId) {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: 'DELETE',
+        headers: this._headers
+      }).then(this._handleResponse);
+    }
+  }
---
+++ scripts/components/PopupWithConfirmation.js
@@ -0,0 +1,21 @@
+import Popup from './Popup.js';
+  
+  export default class PopupWithConfirmation extends Popup {
+    constructor(popupSelector) {
+      super(popupSelector);
+      this._form = this._popup.querySelector('.popup__form');
+    }
+  
+    setSubmitAction(action) {
+      this._handleSubmit = action;
+    }
+  
+    setEventListeners() {
+      super.setEventListeners();
+      this._form.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        this._handleSubmit();
+      });
+    }
+  } 
+  
